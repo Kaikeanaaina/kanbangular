0c683589bb1bfca694e9e082b0cda6b796f3225c
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-02-08 16:53:20 -1000

    added services.js to public/js

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -3,9 +3,16 @@ angular.module('myApp',['ngRoute']);
 var myApp = angular.module('myApp');
 
 myApp
-  .config(['$routeProvider', function ($routeProvider){
+  .config(['$routeProvider', '$locationProvider', function ($routeProvider,$locationProvider){
+
+    $locationProvider.html5Mode({
+      enabled:true,
+      requireBase:false
+    });
+
+
     $routeProvider
-      .when('/default', {
+      .when('/', {
         templateUrl : 'views/default.html'
       });
 }]);
