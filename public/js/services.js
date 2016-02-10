a896ca3d0ea7ba07abfa2f46b945c8030a9b7b75
Chaz <cmtlum73@gmail.com>
2016-02-09 17:25:57 -1000

    gutted this whole file and adding in methods slowly

--- public/js/services.js
+++ public/js/services.js
@@ -1,98 +1,78 @@
-(function(){
-  'use strict';
+'use strict';
 
-
-  function toDoService(){
-    this.todos = [
-      {
-        id: 1,
-        title: 'first',
-        prioritySelection: 'low',
-        status:'toDo',
-        created_By: 'Kai',
-        assigned_To: 'Kai'
-      },
-      {
-        id: 2,
-        title: 'second',
-        prioritySelection: 'medium',
-        status:'done',
-        created_By: 'Chaz',
-        assigned_To: 'Charles'
-      }
-    ];
-
+angular.module('myApp')
+  .service('toDoService', ['$http', function ( $http ) {
 
     this.getToDos = function(){
-      return this.todos;
-    };
-
-    this.getToDo = function(id){
-      return todos.filter(function(todo){
-        return this.todo.id===id;
-      })
-      .reduce(function(_,todo){
-        return todo;
-      });
+      return $http.get('http://localhost:3000/api/tasks');
     };
 
     this.addToDos = function(todo){
-      var nextId = this.todos.length+1;
-      todo.id = nextId;
       var new_todo ={
-        id: nextId,
         title: todo.title,
         prioritySelection: todo.prioritySelection,
         status: 'toDo',
         created_By: todo.createdBy,
         assigned_To : todo.assignedTo
-      }
-
-      this.todos.push(new_todo);
-
+      };
+      return $http.post('http://localhost:3000/api/tasks', new_todo);
     };
+  }]);
 
-    this.changeStatus = function(todo){
-      console.log('changing');
-      console.log(todo);
-      if(todo.status==='toDo'){
-        todo.status='inProgress';
-        return todo;
-      }
 
-      if(todo.status==='inProgress'){
-        todo.status='done';
-        return todo;
+//   function toDoService(){
 
-      }
 
-      if(todo.status==='done'){
-        //delete this shit
-      }
-    };
+//
 
-    this.reverseStatusChange = function(todo){
-      console.log('changing');
-      console.log(todo);
-      if(todo.status==='inProgress'){
-        todo.status='toDo';
-        return todo;
-      }
+//     this.getToDo = function(id){
+//       return todos.filter(function(todo){
+//         return this.todo.id===id;
+//       })
+//       .reduce(function(_,todo){
+//         return todo;
+//       });
+//     };
 
-      if(todo.status==='done'){
-        todo.status='inProgress';
-        return todo;
 
-      }
-    };
+//     this.changeStatus = function(todo){
+//       console.log('changing');
+//       console.log(todo);
+//       if(todo.status==='toDo'){
+//         todo.status='inProgress';
+//         return todo;
+//       }
 
-    // this.deleteToDo(todo){
-    //   .getToDo(todo.id)
-    // }
+//       if(todo.status==='inProgress'){
+//         todo.status='done';
+//         return todo;
 
-  }
+//       }
 
-  angular.module('myApp')
-    .service('toDoService', toDoService);
+//       if(todo.status==='done'){
+//         //delete this shit
+//       }
+//     };
 
-})();
+//     this.reverseStatusChange = function(todo){
+//       console.log('changing');
+//       console.log(todo);
+//       if(todo.status==='inProgress'){
+//         todo.status='toDo';
+//         return todo;
+//       }
+
+//       if(todo.status==='done'){
+//         todo.status='inProgress';
+//         return todo;
+
+//       }
+//     };
+
+//     // this.deleteToDo(todo){
+//     //   .getToDo(todo.id)
+//     // }
+
+//   }
+
+
