f0fe268106832dd499b9351e74912b51dd6cad31
Chaz <cmtlum73@gmail.com>
2016-02-09 17:21:49 -1000

    adds body parser, api data, handles get request, post request still WIP

--- server.js
+++ server.js
@@ -1,7 +1,39 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use( express.static( './public' ));
+
+app.use(bodyParser.json());
+
+var tasks = [
+  {
+    id: 1,
+    title: 'first',
+    prioritySelection: 'low',
+    status:'toDo',
+    created_By: 'Kai',
+    assigned_To: 'Kai'
+  },
+  {
+    id: 2,
+    title: 'second',
+    prioritySelection: 'medium',
+    status:'done',
+    created_By: 'Chaz',
+    assigned_To: 'Charles'
+  }
+];
+
+app.get( '/api/tasks', function ( req, res ) {
+  res.json( tasks );
+});
+
+app.post( '/api/tasks', function ( req, res ) {
+  req.body.id = tasks.length+1;
+  tasks.push( req.body );
+  res.json( tasks );
+});
 
 
 app.listen(3000,function(){
