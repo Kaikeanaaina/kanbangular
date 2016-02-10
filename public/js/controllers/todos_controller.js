75e05f19366f13221e8a89265ac91ad07ae4dd51
Chaz <cmtlum73@gmail.com>
2016-02-09 17:29:29 -1000

    attaches methods to  for getting all and posting new data

--- public/js/controllers/todos_controller.js
+++ public/js/controllers/todos_controller.js
@@ -1,6 +1,22 @@
 'use strict';
 
 angular.module('myApp')
-.controller('ToDosController', ['$scope', 'toDoService',function($scope,toDoService){
+.controller('ToDosController', ['$scope', 'toDoService',
+  function( $scope, toDoService ){
   $scope.toDoService = toDoService;
+
+  toDoService.getToDos()
+    .success( function ( data ) {
+      $scope.tasks = data;
+    });
+
+  $scope.addTodos=function(new_todo) {
+    console.log('8===D{()}');
+    toDoService.addTodos( todo )
+      .success( function ( response ) {
+        console.log(response);
+        $scope.tasks.push( todo );
+      });
+  };
+
 }]);
