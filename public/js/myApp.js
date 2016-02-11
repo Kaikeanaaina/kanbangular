f1313c397b9a9af28e90c885d9ec7a90da5a0732
Chaz <cmtlum73@gmail.com>
2016-02-10 16:45:53 -1000

    adds back in

--- public/js/myApp.js
+++ public/js/myApp.js
@@ -3,13 +3,13 @@ angular.module('myApp',['ngRoute']);
 var myApp = angular.module('myApp');
 
 myApp
-  .config([ '$routeProvider',
-    function ($routeProvider){
+  .config([ '$routeProvider', '$locationProvider',
+    function ($routeProvider, $locationProvider){
 
-    // $locationProvider.html5Mode({
-    //   enabled:true,
-    //   requireBase:false
-    // });
+    $locationProvider.html5Mode({
+      enabled:true,
+      requireBase:false
+    });
 
 
     $routeProvider
