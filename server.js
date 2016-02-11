b62903616eba157662d76e6fb511d2fd4f1938ca
Chaz <cmtlum73@gmail.com>
2016-02-10 16:47:23 -1000

    adds routing middleware and

--- server.js
+++ server.js
@@ -8,41 +8,18 @@ var loggedInChecker = false;
 
 var db = require('./models');
 var User = db.User;
+var Task = db.Task;
 
-app.use('/users', require('./queries/queries.js'));
+app.use('/users', require('./queries/users.js'));
+app.use('/tasks', require('./queries/tasks.js'));
 
 app.use( express.static( './public' ));
 
 app.use(bodyParser.urlencoded({extended:true}));
 
-// var tasks = [
-//   {
-//     id: 1,
-//     title: 'first',
-//     prioritySelection: 'low',
-//     status:'toDo',
-//     created_By: 'Kai',
-//     assigned_To: 'Kai'
-//   },
-//   {
-//     id: 2,
-//     title: 'second',
-//     prioritySelection: 'medium',
-//     status:'done',
-//     created_By: 'Chaz',
-//     assigned_To: 'Charles'
-//   }
-// ];
-
-// app.get( '/api/tasks', function ( req, res ) {
-//   res.json( tasks );
-// });
-
-// app.post( '/api/tasks', function ( req, res ) {
-//   req.body.id = tasks.length+1;
-//   tasks.push( req.body );
-//   res.json( req.body );
-// });
+app.get('*', function(req,res) {
+ res.sendFile('/public/index.html', { root : __dirname });
+});
 
 // app.delete( '/api/tasks/:id', function( req, res){
 //   deletedOne = tasks.splice(req.params.id-1,1);
