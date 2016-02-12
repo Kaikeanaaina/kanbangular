ad65b5d4f4bd38522689306aa429971177193ac8
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-02-11 18:30:13 -1000

    returned the task.destroy

--- queries/tasks.js
+++ queries/tasks.js
@@ -70,14 +70,14 @@ router.put( '/left', function ( req, res ) {
 router.delete('/:id',function( req , res){
   Task.findById(req.params.id)
   .then(function(data){
-    Task.destroy({
+    return Task.destroy({
       where : {
         id: req.params.id
       }
     })
   })
   .then( function ( task ) {
-    res.json( task );
+    res.json( 'card '+ req.params.id + ' deleted' );
   });
 });
 
