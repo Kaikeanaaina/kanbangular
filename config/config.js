3ab157d15cf42c1d94d0aacecc2727f9dceee71e
Chaz <cmtlum73@gmail.com>
2016-02-11 16:16:35 -1000

    code cleanup

--- config/config.js
+++ config/config.js
@@ -1,10 +1,6 @@
 module.exports = {
   'PORT' : 3000,
   'SESSION' : {
-    "secret" : "Keyboard cat"
-  },
-  'CREDENTIALS' : {
-    "USERNAME" : "Bob",
-    "PASSWORD" : "superSecret"
+    "secret" : "Fruit Loops ;adsosvi4wvh;3 o/;jsohagia"
   }
 };
